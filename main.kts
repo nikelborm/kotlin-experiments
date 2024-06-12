a6cf59e5ff2ec3d4dde146f74fc2a1143d7c0533
nikelborm <kolya007.klass@gmail.com>
2024-06-12 05:40:51 +0300

    save

--- main.kts
+++ main.kts
@@ -1,12 +1,19 @@
 fun main(args: Array<String>) {
-    if (args.size != 2) {
+    if (args.size !== 2) {
         println("go fuck yourself");
         return;
     }
-    println("Hello, World! sarg: ${args[2]}");
+
+    for (arg in args) {
+        for (charr in arg) {
+            println(charr)
+        }
+    }
+
+    // println("Hello, World! sarg: ${args[2]}");
 }
 
 val test = arrayOf("asd", "asdd2")
 val test2 = arrayOf<String>()
 
-main(test2)
+main(test)
