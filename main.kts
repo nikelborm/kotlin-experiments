5ebe719b47ea0a79f8bf553d52ae150e9084a465
nikelborm <kolya007.klass@gmail.com>
2024-06-12 06:40:39 +0300

    save

--- main.kts
+++ main.kts
@@ -1,24 +1,7 @@
-fun main(args: Iterable<String>) {
-    var size = 0;
-    for (i in args) {
-        size += 1;
-    }
-    if (size !== 2) {
-        println("go fuck yourself");
-        return;
-    }
+class Solution {
+    fun isPerfectSquare(num: Int): Boolean {
 
-    for (arg in args) {
-        for (charr in arg) {
-            val ternaryCheck = if (charr === 'a') charr else charr.toString().repeat(3)
-            println("values: ${ternaryCheck}")
-        }
     }
-
-    // println("Hello, World! sarg: ${args[2]}");
 }
 
-val test = arrayOf("asd", "asdd2")
-val test2 = arrayOf<String>()
-
-main(test.asIterable())
+val sol = Solution
