113557a94759ed2b1671fda71271b88596a59ed7
nikelborm <kolya007.klass@gmail.com>
2024-06-19 06:13:06 +0300

    save

--- main.kts
+++ main.kts
@@ -1,34 +1,79 @@
-class Solution {
-  fun checkRecord(s: String): Boolean {
-    var timesWasAbsent: Byte = 0
-    var timesWasLate: Byte = 0
-    val consecutiveLatesNeededToBeBanned: Byte = 3
-    val absencesNeededToBeBanned: Byte = 2
+class TreeNode(var `val`: Int) {
+  var left: TreeNode? = null
+  var right: TreeNode? = null
+}
 
-    for (l in s) {
-      if(l === 'A') timesWasAbsent++
-      if(timesWasAbsent === absencesNeededToBeBanned) return false
-      if(l === 'L')
-        timesWasLate++
-      else
-        timesWasLate = 0.toByte()
-      if(timesWasLate === consecutiveLatesNeededToBeBanned) return false
+fun node(left: TreeNode?, value: Int, right: TreeNode?): TreeNode {
+  val newNode = TreeNode(value)
+  newNode.left = left
+  newNode.right = right
+  return newNode
+}
+
+
+
+class Solution {
+  fun step(node: TreeNode, traceToParentStack: MutableList<TreeNode>, ret: MutableList<Int>): TreeNode {
+    if(node.left !== null) {
+      traceToParentStack.add(node)
+      return node.left
     }
-    return true
+    if(node.right !== null) {
+      traceToParentStack.add(node)
+      return node.right
+    }
+    ret.add(node.`val`)
+
+
+    var leftLeaf = root
+    while(leftLeaf?.left !== null) {
+      stack.add(leftLeaf)
+      leftLeaf = leftLeaf!!.left
+    }
+    return leftLeaf
+  }
+
+  fun postorderTraversal(root: TreeNode?): List<Int> {
+    if (root === null) return listOf<Int>();
+
+    val mutableList = mutableListOf<Int>();
+
+    val stack = mutableListOf<TreeNode>();
+    var currentNode = root;
+
+    // going down
+    while(currentNode?.left !== null) {
+      stack.add(currentNode)
+      currentNode = currentNode!!.left
+    }
+    println(stack)
+
+    // while(currentNode !== root) {
+
+    // }
+
+
+    return mutableList.toList()
+
   }
 }
 
 val sol = Solution()
 
 val test = mapOf(
-  "PPALLP" to true,
-  "PPALLL" to false
+  node(null, 1, node(TreeNode(3), 2, null)) to listOf(3, 2, 1),
+
+  node(
+    node(TreeNode(1), 3, TreeNode(2)),
+    7,
+    node(TreeNode(4), 6, TreeNode(5))
+  ) to listOf(1, 2, 3, 4, 5, 6, 7)
 )
 
 for (i in test) {
-  val result = sol.checkRecord(i.key);
+  val result = sol.postorderTraversal(i.key);
 
-  if (result !== i.value)
-    throw Exception("checkRecord(${i.key})=${result}. Expected ${i.value}")
-  println("checkRecord(${i.key}) is fine ❤️ ")
+  // if (result !== i.value)
+  //   throw Exception("postorderTraversal(${i.key})=${result}. Expected ${i.value}")
+  // println("postorderTraversal(${i.key}) is fine ❤️ ")
 }
